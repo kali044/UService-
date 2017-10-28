c77abcf4f154cd352b4b5e42c2635a3771395439
jzhao1223 <jzhao1223@gmail.com>
2017-10-28 15:09:47 -0400

    added carpool to admin.py

--- TEAMRICHARDS/USERVICE/admin.py
+++ TEAMRICHARDS/USERVICE/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 #Register your models here.
 
-from .models import Profile
+from .models import Profile, Carpool
 
 # admin.site.register(Profile)
 
@@ -10,6 +10,11 @@ from .models import Profile
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'email', 'rating', 'review')
     fields = ['last_name', 'first_name', 'email', 'rating', 'review']
+
+@admin.register(Carpool) 
+class CarpoolAdmin(admin.ModelAdmin):
+	list_display = ('creator', 'title', 'destination', 'date', 'cost')
+	fields = ['creator', 'title', 'destination', 'date', 'cost']
 
 # Register the admin class with the associated model
 admin.site.register(Profile, ProfileAdmin)
