acf963c243f42b7f37dc02c2d33ea6a0595be180
jzhao1223 <jzhao1223@gmail.com>
2017-10-28 14:17:42 -0400

    added Carpool model

--- TEAMRICHARDS/USERVICE/models.py
+++ TEAMRICHARDS/USERVICE/models.py
@@ -1,30 +1,47 @@
 from django.db import models
+from decimal import Decimal
 
 # Create your models here.
 
-class MyModelName(models.Model):
+class Carpool(models.Model):
     """
-    A typical class defining a model, derived from the Model class.
+    Model representing a carpool.
     """
+    title = models.CharField(max_length=200)
+    creator = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
+    description = models.TextField(max_length=1000, help_text="Enter a brief desciption of the service")
+    destination = models.CharField(max_length=200)
+    date = models.DateField(null=True, blank=True)
+    cost = models.DecimalField(max_digits=4, decimal_places=2, default=Decimal('0.00'))
+    request = models.BooleanField(default=False)
+    offer = models.BooleanField(default=False)
 
-    # Fields
-    my_field_name = models.CharField(max_length=20, help_text="Enter field documentation")
-    ...
-
-    # Metadata
-    class Meta: 
-        ordering = ["-my_field_name"]
-
-    # Methods
-    def get_absolute_url(self):
-         """
-         Returns the url to access a particular instance of MyModelName.
-         """
-         return reverse('model-detail-view', args=[str(self.id)])
-    
-    def __str__(self):
+    def edit_title(self, new_title):
         """
-        String for representing the MyModelName object (in Admin site etc.)
+        Edit title for the Carpool.
         """
-        return self.field_name
+        self.title = new_title
 
+    def edit_description(self, new_desciption):
+        """
+        Edit desciption for the Carpool.
+        """
+        self.desciption = new_desciption
+
+    def edit_destination(self, new_destination):
+        """
+        Edit destination for the Carpool.
+        """
+        self.destination = new_destination
+
+    def edit_date(self, new_date):
+        """
+        Edit date for the Carpool.
+        """
+        self.date = new_date
+
+    def edit_cost(self, new_cost):
+        """
+        Edit cost for the Carpool.
+        """
+        self.cost = new_cost
