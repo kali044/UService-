13af88012d7aca67d9f8b4d7ccb85875c3447263
timmliew <liew_timothy@hotmail.com>
2017-10-28 13:23:19 -0400

    Profile is done

--- TEAMRICHARDS/USERVICE/admin.py
+++ TEAMRICHARDS/USERVICE/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Service, Profile
+
+
+# Define the admin class
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelProfile):
+    list_display = ('last_name', 'first_name', 'email', 'rating', 'review')
+    fields = ['first_name', 'last_name', 'email', 'rating', 'review']
+
+admin.site.register(Profile, ProfileAdmin)
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Service)
+class Service(admin.ModelService):
+    list_display = ('title')
--- TEAMRICHARDS/USERVICE/models.py
+++ TEAMRICHARDS/USERVICE/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+from django.urls import reverse #Used to generate URLs by reversing the URL patterns
+
+class Carpool(models.Model):
+    """
+    Model representing a book genre (e.g. Science Fiction, Non Fiction).
+    """
+    name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
+
+    def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """
+        return self.name
+
+# class Profile(models.Model):
+#     """
+#     Model representing an author.
+#     """
+#     first_name = models.CharField(max_length=100)
+#     last_name = models.CharField(max_length=100)
+#     email = models.CharField(max_length=100)
+#     rating = models.CharField(max_length=100)
+#     review = models.CharField(max_length=100)
+#
+#     def get_absolute_url(self):
+#         """
+#         Returns the url to access a particular author instance.
+#         """
+#         return reverse('profile-detail', args=[str(self.id)])
+#
+#
+#     def __str__(self):
+#         """
+#         String for representing the Model object.
+#         """
+#         return '%s, %s' % (self.last_name, self.first_name)
