d1afbfb79fae0c04edcc9594fed5be844a4eaf4d
brandonnlam <brandonnlam@gmail.com>
2017-10-28 15:12:19 -0400

    activityadmin

--- TEAMRICHARDS/USERVICE/admin.py
+++ TEAMRICHARDS/USERVICE/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 #Register your models here.
 
-from .models import Profile
+from .models import Profile,Activity,Carpool,Tutor,Textbook_Trading
 
 # admin.site.register(Profile)
 
@@ -11,5 +11,10 @@ class ProfileAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'email', 'rating', 'review')
     fields = ['last_name', 'first_name', 'email', 'rating', 'review']
 
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+	list_display = ('title','creator','date', 'activity')
+	fields = ['title','creator','date', 'activity']
 # Register the admin class with the associated model
 admin.site.register(Profile, ProfileAdmin)
+
--- TEAMRICHARDS/USERVICE/models.py
+++ TEAMRICHARDS/USERVICE/models.py
@@ -1,7 +1,50 @@
 from django.db import models
 from decimal import Decimal
+from django.urls import reverse #Used to generate URLs by reversing the URL patterns
 
 # Create your models here.
+
+class Profile(models.Model):
+    """
+    Model representing a profile.
+    """
+    first_name = models.CharField(max_length=100, help_text="First Name")
+    last_name = models.CharField(max_length=100, help_text="Last Name")
+    email = models.CharField(max_length=100, help_text="johnorjane_doe@email.com")
+    review = models.CharField(max_length=500, help_text="Review")
+    RATING = (
+        ('vb', 'Very Bad'),
+        ('b', 'Bad'),
+        ('g', 'Good'),
+        ('vg', 'Very Good'),
+    )
+
+    rating = models.CharField(max_length=1, choices=RATING, blank=True, default='vb', help_text='Rate Me')
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular author instance.
+        """
+        return reverse('profile-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return '%s, %s' % (self.last_name, self.first_name)
+
+    #not sure if this works
+    def edit_profile(newfirstname, newlastname, newemail):
+        # Create a new record using the model's constructor.
+        firstname = Profile(first_name = newfirstname)
+        lastname = Profile(last_name = newlastname)
+        email = Profile(first_name = newemail)
+        # Save the object into the database.
+        firstname.save()
+        lastname.save()
+        email.save()
+        return
+
 class Activity(models.Model):
     """
     Model representing a book (but not a specific copy of a book).
@@ -15,19 +58,19 @@ class Activity(models.Model):
     offer = models.BooleanField(default=False)
 
     def modifyTitle(self, title2):
-    	self.title = title2
+        self.title = title2
 
     def modifyDescription(self,desc2):
-    	self.description = desc2
-    	
+        self.description = desc2
+
 
     def modifyDate(self,date2):
         self.date = date2
-        
+
 
     def modifyActivity(self,act2):
         self.activity = act2
-        
+
 
 class Carpool(models.Model):
     """
@@ -76,8 +119,8 @@ class Tutor(models.Model):
     """
     Model representing a tutor service
     """
-
     title = models.CharField(max_length=200, help_text="Enter a tutor service title" )
+    creator = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
     description = models.TextField(max_length=1000, help_text = "Enter a brief description of the tutor")
     date = models.DateField(null=True, blank=True)
     cost = models.DecimalField(max_digits=6, decimal_places=2, help_text="Enter the of each tutor session")
@@ -86,23 +129,23 @@ class Tutor(models.Model):
     offer = models.BooleanField(default=False)
 
     def editTitle(newTitle, self):
-        if (newTitle!=NULL)
+        if (newTitle!=NULL):
             self.title = newTitle
 
     def editDescription(newDesc, self):
-        if (newDesc != NULL)
+        if (newDesc != NULL):
             self.description = newDesc
 
     def editDate(newDate, self):
-        if (newDate != NULL)
+        if (newDate != NULL):
             self.date = newDate
 
     def editCost(newCost, self):
-        if (newCost != NULL)
+        if (newCost != NULL):
             self.cost = newCost
 
     def editSubject(newSubject, self):
-        if (newSubject != NULL)
+        if (newSubject != NULL):
             self.subject = newSubject
 
     def __str__(self):
@@ -114,10 +157,6 @@ class Tutor(models.Model):
 # Create your models here.
 
 class Textbook_Trading(models.Model):
-    """
-    A typical class defining a model, derived from the Model class.
-    """
-
     # Fields
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
@@ -140,7 +179,7 @@ class Textbook_Trading(models.Model):
         self.date=newDate
     def edit_cost(self,newCost):
         self.cost=newCost
-
+        
     # Methods
     def get_absolute_url(self):
          """
