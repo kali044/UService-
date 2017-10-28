87ace82065667171c4abce230cdc12200ac1e60b
Weihai Chen <weihaichen@umass.edu>
2017-10-28 14:15:15 -0400

    add model Textbook

--- TEAMRICHARDS/USERVICE/models.py
+++ TEAMRICHARDS/USERVICE/models.py
@@ -1,30 +1,45 @@
 from django.db import models
+from decimal import Decimal
 
 # Create your models here.
 
-class MyModelName(models.Model):
+class Textbook_Trading(models.Model):
     """
     A typical class defining a model, derived from the Model class.
     """
 
     # Fields
-    my_field_name = models.CharField(max_length=20, help_text="Enter field documentation")
-    ...
-
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    description = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
+    creator=models.ForeignKey(Profile,on_delete=models.SET_NULL,null=True)
+    date=models.DateField(null=True,blank=True)
+    cost=models.DecimalField(max_digits=4,decimal_places=2,default=Decimal('0,00'))
+    request=models.BooleanField(default=False)
+    offer=models.BooleanField(default=False)
     # Metadata
-    class Meta: 
-        ordering = ["-my_field_name"]
+    def edit_title(self,newTitle):
+        self.title=newTitle
+    def edit_author(self,new_Author):
+        self.author=new_Author
+    def edit_description(self,new_description):
+        self.description=new_description
+    def edit_creator(self,new_creator):
+        self.creator=new_creator
+    def edit_data(self,newDate):
+        self.date=newDate
+    def edit_cost(self,newCost):
+        self.cost=newCost
 
     # Methods
     def get_absolute_url(self):
          """
          Returns the url to access a particular instance of MyModelName.
          """
-         return reverse('model-detail-view', args=[str(self.id)])
-    
+         return reverse('Textbook details', args=[str(self.id)])
+
     def __str__(self):
         """
-        String for representing the MyModelName object (in Admin site etc.)
+        String for representing the Model object.
         """
-        return self.field_name
-
+        return self.title
