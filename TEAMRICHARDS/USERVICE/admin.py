911dac93a3a054d5920acc282695849e85e468a1
Weihai Chen <weihaichen@umass.edu>
2017-10-28 15:22:58 -0400

    add textbook admin

--- TEAMRICHARDS/USERVICE/admin.py
+++ TEAMRICHARDS/USERVICE/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from .models import Textbook_Trading
 #Register your models here.
 
-from .models import Profile, Activity, Carpool, Tutor, Textbook_Trading
-
+from .models import Profile,Textbook_Trading
 
 # admin.site.register(Profile)
 
@@ -15,22 +13,7 @@ class ProfileAdmin(admin.ModelAdmin):
 # Register the admin class with the associated model
 admin.site.register(Profile, ProfileAdmin)
 
-<<<<<<< HEAD
 @admin.register(Textbook_Trading)
 class TextbookAdmin(admin.ModelAdmin):
     list_display=('title','author','cost','creator')
     fields=['title','author','cost','creator']
-    inlines=[TextbookInLine]
-class TextbookInLine(admin.TabularInline):
-    model = Textbook_Trading
-=======
-@admin.register(Carpool) 
-class CarpoolAdmin(admin.ModelAdmin):
-	list_display = ('creator', 'title', 'destination', 'date', 'cost')
-	fields = ['creator', 'title', 'destination', 'date', 'cost']
-
-@admin.register(Activity)
-class ActivityAdmin(admin.ModelAdmin):
-	list_display = ('title','creator','date', 'activity')
-	fields = ['title','creator','date', 'activity']
->>>>>>> 90f33146bb6e2f0da34ae5e8ff52ecc5c95a28d0
