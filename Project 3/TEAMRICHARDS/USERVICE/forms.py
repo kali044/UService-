74e33667043686d07798e7fcfcff03c179d0a96b
Marcus Li <kayuli@umass.edu>
2017-11-23 21:23:16 -0500

    form template in addOffer.html and addRequest.html and created forms.py

---
+++ Project 3/TEAMRICHARDS/USERVICE/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+    
+class EditTitleForm(forms.Form):
+
+class EditDateForm(forms.Form):
+
+class EditFieldForm(forms.Form):
+
+class EditCostForm(forms.Form):
+
+class EditDescriptionForm(forms.Form):
+
---
+++ Project 3/TEAMRICHARDS/USERVICE/static/js/edit.js
@@ -0,0 +1,1 @@
+document.getElementById('focusme').focus();
