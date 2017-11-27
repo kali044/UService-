e07938b591bc62416afe2c2f6f746eabc6824478
timmliew <liew_timothy@hotmail.com>
2017-11-26 19:37:47 -0500

    added code for login and logout

--- Project 2/TEAMRICHARDS/TEAMRICHARDS/settings.py
+++ Project 2/TEAMRICHARDS/TEAMRICHARDS/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'TEAMRICHARDS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['./templates',],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Redirect to home URL after login (Default redirects to /accounts/profile/)
-LOGIN_REDIRECT_URL = '/'
